84aab1ebb63af046d6528b0378f7ce1344953003
Vitaliy Eremeev <evit.viter@gmail.com>
2023-05-08 05:40:48 +0700

    не доделан

---
+++ data/users.py
@@ -0,0 +1,8 @@
+import dataclasses
+
+@dataclasses.dataclass
+class User:
+    full_name: str
+    email: str
+
+
---
+++ tests/elements_test.py
@@ -0,0 +1,6 @@
+import os
+
+
+def resource_path(file_name):
+    return os.path.abspath(f'../QA_5_9/resources/{file_name}')
+
